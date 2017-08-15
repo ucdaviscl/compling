eac8cfe15586516047f2b755107f38b7c18cef8e
Richard Kim <khgkim@ucdavis.edu>
2017-08-15 12:50:50 -0700

    modified tokenizer

---
+++ re.py
@@ -0,0 +1,53 @@
+"""
+    Created on August 15 2017
+
+    Regular expression
+"""
+
+import os, logging, sys, datetime, time
+import glob, codecs, re
+import nltk
+
+from nltk.corpus import PlaintextCorpusReader
+
+start_time = time.time()
+
+timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+print timestamp
+
+logging.basicConfig(
+  format = '%(asctime)s : %(levelname)s : %(message)s',
+  level = logging.INFO
+)
+
+# Specify paths
+dump_path = '/media/khgkim/data/khgkim/compling/test2/'
+
+os.chdir(dump_path)
+
+# For each directory
+for directory in glob.glob("*"):
+  # Get all wiki articles
+  wiki = PlaintextCorpusReader(directory, 'wiki_.*')
+  # Replace characters using regular expression
+  for files in wiki.fileids():
+    s = codecs.open(directory + '/' + files, encoding='utf-8')
+    contents = s.read()
+    s.close()
+    contents = re.sub('<[^>]*>(.*\r?\n){2}', '', contents) # Beginning XML tag + article title
+    contents = re.sub('<[^>]*>', '', contents) # Ending XML tag
+    # Find all digit occurences 
+    for digits in re.findall('((?<=\W)|^)(\d+(?!(\-*[a-zA-Z]\-*\s*))(?=\W))', contents):
+      temp = re.sub('\d', '9', digits[1]) # Placeholder for matching groups
+      contents = re.sub(digits[1], temp, contents) # Replace
+      del temp
+    contents = contents.encode('utf-8')
+    # Save changes to the wiki article
+    f = open(directory + "/" + files, 'w')
+    f.write(contents)
+    f.close()
+    del contents 
+
+# Print execution time
+print("--- Execution time: %s minutes ---" % ((time.time() - start_time)/60))
--- tokenizer.py
+++ tokenizer.py
@@ -21,7 +21,7 @@ logging.basicConfig(
 )
 
 # Specify paths
-dump_path = '/media/khgkim/data/khgkim/compling/text/'
+dump_path = '/media/khgkim/data/khgkim/compling/test/'
 token_path = '/media/khgkim/data/khgkim/compling/'
 filename = 'tokenizer_tokens.txt'
 filename2 = 'tokenizer_tokens2.txt'
@@ -37,26 +37,10 @@ if not (os.path.isfile(token_path + filename)):
   for directory in glob.glob("*"):
     # Get all wiki articles
     wiki = PlaintextCorpusReader(directory, 'wiki_.*')
-    # Replace characters using regular expression
-    for files in wiki.fileids():
-      contents = codecs.open(directory + '/' + files, encoding='utf-8').read()
-      contents = re.sub('<[^>]*>(.*\r?\n){2}', '', contents) # Beginning XML tag + article title
-      contents = re.sub('<[^>]*>', '', contents) # Ending XML tag
-      # Find all digit occurences 
-      for digits in re.findall('((?<=\W)|^)(\d+(?!(\-*[a-zA-Z]\-*\s*))(?=\W))', contents): 
-        temp = re.sub('\d', '9', digits[1]) # Placeholder for matching groups
-        contents = re.sub(digits[1], temp, contents) # Replace
-      contents = contents.encode('utf-8')
-      # Save changes to the wiki article
-      f = open(directory + "/" + files, 'w')
-      f.write(contents)
-      f.close()
-      del contents 
     # Tokenize articles
     tok_corp = []
     tok_corp = wiki.words(wiki.fileids())
     # Save tokens to tokenizer_tokens.txt and tok_list
-    f = codecs.open(token_path + filename, "a+", "utf-8")
     for word in tok_corp:
       tok_list.append(word)
       f.write(word + " ")
@@ -64,6 +48,7 @@ if not (os.path.isfile(token_path + filename)):
     del tok_corp 
     del wiki
     f.close()
+  print "Finished tokenizer_tokens.txt"
   # Replace UNK tokens based on frequency and save to tokenizer_tokens2.txt
   if not (os.path.isfile(token_path + filename2)):
     f = codecs.open(token_path + filename2, "a+", "utf-8")
@@ -74,6 +59,7 @@ if not (os.path.isfile(token_path + filename)):
       else: 
         f.write(word + " ")
     del tok_list
+    del fdist
     f.close()
   else:
     print "Output message: File tokenizer_tokens2.txt already exists!"
